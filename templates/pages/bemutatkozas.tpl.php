34b17404acd6e259c064dc90963cc089924aa496
BaloghMln20040301 <BaloghMln.20040301@icloud.com>
2026-04-13 17:15:53 +0200

    Word fajl módosítás

---
+++ templates/pages/bemutatkozas.tpl.php
@@ -0,0 +1,9 @@
+<H2>Bemutatkozás</H2>
+
+<p>
+A Balaton Magyarország legnagyobb tava, amely nemcsak a pihenés és a turizmus egyik központja, hanem a hajózás szempontjából is kiemelkedő jelentőséggel bír. A tó partján számos kikötő és hajóállomás található, amelyek lehetővé teszik a látogatók számára, hogy felfedezzék a Balaton szépségeit vízről is. A hajózás során gyönyörködhetünk a Tihanyi-félsziget látványában, a part menti városok panorámájában, valamint a változatos vízfelületben, amely minden évszakban más arcát mutatja. A balatoni hajózás nemcsak közlekedési eszköz, hanem egyben élmény is, amely hozzájárul a tó varázsához és vonzerejéhez.
+</p>
+
+<p>
+    Weboldalunk célja, hogy bemutassa a Balaton hajózási világát, a legismertebb hajótipusait, valamint érdekességeket és hasznos információkat nyújtson a látogatóknak. Legyen szó akár egy romantikus sétahajózásról, egy családi kirándulásról vagy egy izgalmas vitorlázásról, nálunk mindenki megtalálhatja a számára legmegfelelőbb hajózási lehetőséget a Balatonon.
+</p>
