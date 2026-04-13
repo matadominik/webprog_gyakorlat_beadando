3e8d1e3495b5d559b47a12ada5c065126f160884
mataldominik <mataldominik@gmail.com>
2026-04-13 21:09:19 +0200

    Kepek menu folytatasa. Design meg mindig bugos, nincs kesz. Feltoltes mukodik, de kell a validacio (login)

--- templates/pages/kepek.tpl.php
+++ templates/pages/kepek.tpl.php
@@ -20,9 +20,12 @@
     
             // képek megjelenítése
             foreach ($kepek as $k) {
-                printf("<img src='images/feltoltott/%s'>",
+                printf("
+                <div class='kep'>
+                <img src='images/feltoltott/%s'>
+                </div>",
                     rawurldecode(basename($k))
-                    );
+                );
             }
         ?>
     </div>
@@ -30,6 +33,22 @@
     <div class="focimek">
         <p>Tölts fel képet te is</p>
     </div>
+
+    <form method="POST" enctype="multipart/form-data">
+        <input type="file" name="image" />
+        <button type="submit" name="submit">Feltöltés</button>
+    </form>
 </body>
 </html>
 
+<?php
+    if(isset($_POST['submit'])) {
+        $fajlnev = $_FILES['image']['name'];
+        $tempnev = $_FILES['image']['tmp_name'];
+        $mappa = 'images/feltoltott/'.$fajlnev;
+
+        if(move_uploaded_file($tempnev, $mappa)) {
+
+        }
+    }
+?>
