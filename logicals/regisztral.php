0ec31dec4d50d5f3c6b92603bb1988ac572c311d
mataldominik <mataldominik@gmail.com>
2026-04-14 19:51:38 +0200

    Log in validacio a kepekhez + lokal/szerver db kapcsolat dinamikussa tetele a kodban

--- logicals/regisztral.php
+++ logicals/regisztral.php
@@ -2,8 +2,14 @@
 if(isset($_POST['felhasznalo']) && isset($_POST['jelszo']) && isset($_POST['vezeteknev']) && isset($_POST['utonev'])) {
     try {
         // Kapcsolódás
-        $dbh = new PDO('mysql:host=localhost;dbname=adatb1_b223va', 'adatb1_b223va', 'zs64GuN?6',
+        if($_SERVER['HTTP_HOST'] === 'localhost') {
+            $dbh = new PDO('mysql:host=localhost;dbname=gyakorlat7', 'root', '',
                         array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        } else {
+            $dbh = new PDO('mysql:host=localhost;dbname=adatb1_b223va', 'adatb1_b223va', 'zs64GuN?6',
+                        array(PDO::ATTR_ERRMODE=>PDO::ERRMODE_EXCEPTION));
+        }
+        
         $dbh->query('SET NAMES utf8 COLLATE utf8_hungarian_ci');
         
         // Létezik már a felhasználói név?
