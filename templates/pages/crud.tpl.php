f147c051cfbac43b7dc06a75e34a2659d7806355
mataldominik <mataldominik@gmail.com>
2026-04-15 22:56:17 +0200

    Adatbazis megjelenitese

--- templates/pages/crud.tpl.php
+++ templates/pages/crud.tpl.php
@@ -20,18 +20,29 @@
                     <th>Műveletek</th>
                 <?php endif; ?>
             </tr>
-
-            <tr>
-                <th>1</th>
-                <th>Balatonjárók Zrt.</th>
-                <th>Siófok</th>
-                <?php if (isset($_SESSION['login'])) : ?>                    
-                    <td>
-                        <a href="modositas">Módosítás</a>
-                        <a href="" class="gomb-eltavolitas">Eltávolítás</a>
-                    </td>
-                <?php endif; ?>
-            </tr>
+            <?php if (isset($crud_hiba)) : ?>
+                <tr>
+                    <td colspan="<?= isset($_SESSION['login']) ? 4 : 3 ?>" class="hiba"><?= htmlspecialchars($crud_hiba) ?></td>
+                </tr>
+            <?php elseif (! empty($tulajdonosok)) : ?>
+                <?php foreach ($tulajdonosok as $tulajdonos) : ?>
+                    <tr>
+                        <th><?= htmlspecialchars($tulajdonos['az']) ?></th>
+                        <td><?= htmlspecialchars($tulajdonos['nev']) ?></td>
+                        <td><?= htmlspecialchars($tulajdonos['varos']) ?></td>
+                        <?php if (isset($_SESSION['login'])) : ?>
+                            <td>
+                                <a href="modositas">Módosítás</a>
+                                <a href="" class="gomb-eltavolitas">Eltávolítás</a>
+                            </td>
+                        <?php endif; ?>
+                    </tr>
+                <?php endforeach; ?>
+            <?php else : ?>
+                <tr>
+                    <td colspan="<?= isset($_SESSION['login']) ? 4 : 3 ?>">Nincs megjeleníthető tulajdonos az adatbázisban.</td>
+                </tr>
+            <?php endif; ?>
         </table>
     </div>
     
